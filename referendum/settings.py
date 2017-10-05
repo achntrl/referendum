30efddaaec1de055d56f7d491ff1032f999e9c86
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2017-10-05 23:08:49 +0200

    move to a library for registration

--- referendum/settings.py
+++ referendum/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- referendum/urls.py
+++ referendum/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^', include('users.urls')),
+    url(r'^', include('registration.backends.simple.urls')),
 ]
